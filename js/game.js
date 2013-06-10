852a0ddf7a11f250210bda71fe785e24fe199676
Andrew Hollenbach <anh7216@rit.edu>
2013-06-10 17:12:01 -0400

    Fixed the fullscreen issue

--- js/game.js
+++ js/game.js
@@ -1,42 +1,42 @@
 //=========================================================================
 // Global Variables
 //=========================================================================
-var canvas = {
-    width:  0,
-    height: 0
-}
+var canvas,b,C,physics,anmSettings;
+const PREDICT_SPAN,DEFAULT_POS;
+function setGlobals() {
+    // Animatron variables
+    b = Builder._$;
+    C = anm.C;
+    PREDICT_SPAN = 1/150;
+    anm.M[C.MOD_COLLISIONS].predictSpan = PREDICT_SPAN;
+    DEFAULT_POS = [0,0];
 
-// Animatron variables
-var b = Builder._$, C = anm.C;
-const PREDICT_SPAN = 1/150;
-anm.M[C.MOD_COLLISIONS].predictSpan = PREDICT_SPAN;
-const DEFAULT_POS = [0,0];
+    // Physics globals
+    physics = {}
 
-// Physics globals
-var physics = {}
-
-var anmSettings = 
-{
-    //"debug"  : true,
-    "mode" : C.M_DYNAMIC,
-    "anim" : {
-        "fps": 50,
-        "width" : canvas.width,
-        "height" : canvas.height
+    anmSettings = 
+    {
+        //"debug"  : true,
+        "mode" : C.M_DYNAMIC,
+        "anim" : {
+            "fps": 50,
+            "width" : canvas.width,
+            "height" : canvas.height
+        }
     }
 }
-
 //=========================================================================
 // Scene Building
 //=========================================================================
 
 function initGame() {
+    canvas = document.getElementById('game-canvas');
     Util.goFullscreen();
-    Util.setCanvasSize();
+    setGlobals();
 
     /**Animatron**/
     var scene = buildScene();
-    var game = createPlayer('game-canvas', anmSettings).load(scene);
+    var game = createPlayer(canvas.id, anmSettings).load(scene);
     game.play();
 
     game.startGame = function() {
@@ -46,39 +46,7 @@ function initGame() {
 }
 
 function buildScene() {
-    var player = b('player');
-
-    const COLOR = {
-        DARK : "#444",
-        LIGHT: "#555"
-    }
-
-    var road = b('road');
-
-    var sky = b('sky');
-    sky.rect([canvas.width/2,canvas.height/8], [canvas.width, canvas.height/4+3]);
-    sky.fill("#669CCC");
-
-    var yOff = canvas.height/4;
-    var grassLeft = b('grass-left');
-    grassLeft.path([ [ 0, 0 ],[ canvas.width/2-10, 0 ],[ 0, canvas.height-yOff-100 ],[ 0, canvas.height-yOff ] ]);
-    grassLeft.fill("#60BF40");
-    var bounds = grassLeft.x.path.bounds();
-    grassLeft.reg([bounds[0],bounds[1]]);
-    grassLeft.move([0,yOff]);
-
-    var grassRight = b('grass-left');
-    grassRight.path([ [ 0, 0 ],[ canvas.width/2-10, 0 ],[ canvas.width/2-10, canvas.height-yOff ],[ canvas.width/2-10, canvas.height-yOff-100 ] ]);
-    grassRight.fill("#60BF40");
-    var bounds = grassRight.x.path.bounds();
-    grassRight.reg([bounds[0],bounds[1]]);
-    grassRight.move([canvas.width/2+10,canvas.height/4]);
-
     var scene = b('scene');
-    scene.add(road);
-    scene.add(grassLeft);
-    scene.add(grassRight);
-    scene.add(sky); // add after road(so it is on top)
     return scene;
 }
 
@@ -87,26 +55,36 @@ function buildScene() {
 //=========================================================================
 
 var Util = {
-    goFullScreen: function() {
-        var docElm = document.documentElement;
-
-        if (docElm.requestFullscreen) {
-            docElm.requestFullscreen();
+    goFullscreen: function() {
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen();
         }
-        else if (docElm.mozRequestFullScreen) {
-            docElm.mozRequestFullScreen();          
+        else if (canvas.mozRequestFullScreen) {
+            canvas.mozRequestFullScreen();          
         }
-        else if (docElm.webkitRequestFullScreen) {
-            docElm.webkitRequestFullScreen();
+        else if (canvas.webkitRequestFullScreen) {
+            canvas.webkitRequestFullScreen();
+        } else {
+            return;
         }
-        $("#button").hide();
+        Util.setCanvasSize("fullscreen");
+    },
+    
+    setCanvasSize: function(type) {
+        var w,h;
+        switch(type) {
+            case "fullscreen": w = screen.width;      h = screen.height;      break;
+            case "windowed"  : w = window.innerWidth; h = window.innerHeight; break;
+        }
+        canvas.width = w;
+        canvas.height = h;
+    }
+}
+window.addEventListener("fullscreenchange", function() {
+    if (document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement) {
+        Util.setCanvasSize("fullscreen");
+    } else { 
+        Util.setCanvasSize("windowed");
     }
     
-    setCanvasSize: function() {
-    canvas.width = document.width;
-    canvas.height = document.height-5;
-
-    var c = document.getElementById("game-canvas");
-    c.width = document.width;
-    c.height = document.height;
-}
+});
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function() {
-    initGame();
-    
+    document.getElementById("button").addEventListener("click", function() {
+        initGame();
+    });
 });
